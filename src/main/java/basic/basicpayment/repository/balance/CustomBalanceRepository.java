8cb2345bf88f0991e2a9a330c6ff1d92a0b56f53
juhyeon <juhyeon1114@gmail.com>
2024-05-20 09:58:56 +0900

    feature : Balance조회 쿼리에 Lock 추가

--- src/main/java/basic/basicpayment/repository/balance/CustomBalanceRepository.java
+++ src/main/java/basic/basicpayment/repository/balance/CustomBalanceRepository.java
@@ -3,6 +3,8 @@ package basic.basicpayment.repository.balance;
 import basic.basicpayment.model.balance.Balance;
 import basic.basicpayment.model.common.BalanceCurrency;
 import basic.basicpayment.repository.common.CustomRepository;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -15,6 +17,7 @@ public interface CustomBalanceRepository extends CustomRepository<Balance, Long>
     )
     Balance findOneOrNull(@Param("id") Long id);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select B from Balance B where B.appUser.id=:userId and B.currency=:currency")
     Balance findOneByUserIdAndCurrency(@Param("userId") Long userId, @Param("currency") BalanceCurrency currency);
 
